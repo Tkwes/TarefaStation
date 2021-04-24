9f231d660fdd09e0325d6aeb68d0c15e9293e5b2
Tkwes <wxnaider@gmail.com>
2021-04-23 23:29:17 -0300

    att
    enviado

---
+++ Assets/AIcontrol.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIcontrol : MonoBehaviour
+{
+    public NavMeshAgent agent;//busca um objeto selecionado pelo usuario
+
+    void Start()
+
+    {
+        // busca no objeto o componente navmesh
+        agent = this.GetComponent<NavMeshAgent>(); 
+    }
+
+    
+}
---
+++ Assets/Agentmanager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agentmanager : MonoBehaviour
+{
+    // cria um list de objetos 
+    GameObject[] agentes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Busca um gamrobject que contenha a tag Ai
+        agentes = GameObject.FindGameObjectsWithTag("Ai"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //condicional de precionar botao
+        if (Input.GetMouseButtonDown(0))
+        {
+            //pega a propriedade hit do raycast
+            RaycastHit hit;
+
+            //detecta se o desparo esta em objeto 
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) 
+            {
+                foreach (GameObject a in agentes)
+                    // seta uma nova direcao para os objetos apartir do hit, ponto de contato 
+                    a.GetComponent<AIcontrol>().agent.SetDestination(hit.point);
+            }
+
+
+        }
+
+
+
+    }
+}
